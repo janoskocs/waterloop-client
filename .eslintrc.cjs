3224403f2a3255a64fd11cf815461f1521f1ac45
Janos Kocs <janos.kocs@outlook.com>
2024-04-16 10:13:54 +0100

    refactor: update project setup with linter, and vitest

---
+++ .eslintrc.cjs
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint', 'react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': 'off',
+  },
+  ignorePatterns: ['node_modules', 'dist', 'build', 'vite.config.ts'],
+};
---
+++ src/App.test.tsx
@@ -0,0 +1,18 @@
+import { describe, it, expect } from 'vitest';
+import { screen, render } from '@testing-library/react';
+import App from './App';
+
+describe('App', () => {
+  it('should render hello world', () => {
+    // Arrange
+    render(<App />);
+    const heading = 'Hello world!';
+    // Act
+    // Expect
+    expect(
+      screen.getByRole('heading', {
+        level: 1,
+      }),
+    ).toHaveTextContent(heading);
+  });
+});
