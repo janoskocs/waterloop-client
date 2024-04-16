07b6ab6ca3191ee8233c7cb1cbea1ae708d9750e
Janos Kocs <janos.kocs@outlook.com>
2024-04-16 10:25:25 +0100

    fix: test fixing the line endings

--- src/App.tsx
+++ src/App.tsx
@@ -19,10 +19,16 @@ function App() {
       <h1>Hello world!</h1>
       <div className="card">
         <button type="button" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count is
+          {' '}
+          {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit
+          {' '}
+          <code>src/App.tsx</code>
+          {' '}
+          and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
